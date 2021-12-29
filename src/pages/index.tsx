ccd51fb78e8ce33334ce84770ead656469a210da
Rodrigo De Oliveira <casa.rodrigo@gmail.com>
2021-12-29 16:51:08 -0300

    Ajuste

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,7 @@ export default function Home({ product }: HomeProps) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const price = await stripe.prices.retrieve('price_1IVhtPEr8Nl1t46KAhq5JOHw')
+  const price = await stripe.prices.retrieve('price_1KC5UsCI0QoW6bPGbV6Kxcmh')
 
   const product = {
     priceId: price.id,
